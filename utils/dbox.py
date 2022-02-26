b29277d57dc7c2dd97f60b3dee46efc60c512fb7
Gwyn Hannay <gwyn@hannay.id.au>
2022-02-26 10:10:15 +0800

    connect to dropbox

---
+++ utils/dbox.py
@@ -0,0 +1,30 @@
+import dropbox
+import configparser
+
+from dropbox import DropboxOAuth2FlowNoRedirect
+
+config = configparser.ConfigParser()
+config.read('config/keys.ini')
+dropbox_conf = config['DROPBOX']
+
+def connect():
+    auth_flow = DropboxOAuth2FlowNoRedirect(dropbox_conf['KEY'], dropbox_conf['SECRET'])
+
+    authorize_url = auth_flow.start()
+    print("1. Go to: " + authorize_url)
+    print("2. Click \"Allow\" (you might have to log in first).")
+    print("3. Copy the authorization code.")
+    auth_code = input("Enter the authorization code here: ").strip()
+
+    try:
+        oauth_result = auth_flow.finish(auth_code)
+    except Exception as e:
+        print('Error: %s' % (e,))
+        exit(1)
+
+    with dropbox.Dropbox(oauth2_access_token=oauth_result.access_token) as dbx:
+        dbx.users_get_current_account()
+        print("Successfully set up client!")
+
+if __name__ == "__main__":
+    connect()
